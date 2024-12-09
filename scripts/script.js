9313ec6fb5b56c3c0578c5816304cee3c23bd786
Robeill <robeldesalegn07@gmail.com>
2024-12-10 00:50:49 +0300

    Add profile selection page

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const manageProfilesBtn = document.querySelector('.manage-profiles button');
+
+manageProfilesBtn.addEventListener('click', () => {
+  console.log('Manage Profiles button clicked');
+});
