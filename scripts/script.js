eedc9b8f26a86552bc1f817506eef1a119bb04c2
Robeill <robeldesalegn07@gmail.com>
2024-12-12 21:28:24 +0300

    Add landing page

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-const manageProfilesBtn = document.querySelector('.manage-profiles button');
-
-manageProfilesBtn.addEventListener('click', () => {
-  console.log('Manage Profiles button clicked');
-});
